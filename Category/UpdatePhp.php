6bba3a52ebb8eb44f0374e6b601738d062ccf9f8
Jyo <iamjothees@gmail.com>
2021-12-30 09:19:33 +0530

    Pages linked & php codings added

---
+++ Category/UpdatePhp.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        Hello in UpdatePHP body
+    </body>
+<?php
+            echo "Hello World in UpdatePHP";
+            $title = $_POST['title'];
+            echo $title;
+?>
+</html>
